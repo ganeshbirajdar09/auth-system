743e44dff22e262607f1eda1494514324f4282c3
Ganesh <ganesh.birajdar.1930@gmail.com>
2022-01-20 14:32:42 +0530

    auth system

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = require("./app");
 const { PORT } = process.env;
 
 app.listen(PORT, (req, res) =>
-  console.log(`Server is Up and running at port ${PORT}`)
+  console.log(`Server is Up and Running at port ${PORT}`)
 );
