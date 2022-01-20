baf395290c669aacb3e77bdc3de291bbb5053240
Ganesh <ganesh.birajdar.1930@gmail.com>
2022-01-20 14:31:52 +0530

    auth system

---
+++ app.js
@@ -0,0 +1,122 @@
+require("dotenv").config();
+require("./config/database").connect();
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+var cookieParser = require("cookie-parser");
+const User = require("./model/user");
+const auth = require("./middleware/auth");
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("<h1> Hello from auth system - LCO</h1>");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const { firstname, lastname, email, password } = req.body;
+
+    if (!(email && password && firstname && lastname)) {
+      res.status(400).send("All fields are required");
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      res.status(401).send("User already exists");
+    }
+
+    const myEncPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      firstname,
+      lastname,
+      email: email.toLowerCase(),
+      password: myEncPassword,
+    });
+
+    //token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+
+    user.token = token;
+    //update or not
+
+    //handle password situation
+    user.password = undefined;
+
+    //send token or send just success yes and redirect - choice
+    res.status(201).json(user);
+  } catch (error) {
+    console.log("ERROR:", error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!(email && password)) {
+      res.status(400).send("Field is missing");
+    }
+
+    const user = await User.findOne({ email });
+
+    // if(!user){
+    //   res.status(400).send("You are not registered in our app")
+    // }
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.SECRET_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      user.token = token;
+      user.password = undefined;
+      // res.status(200).json(user); FIXME:
+
+      // if you want to use cookies
+      const options = {
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+      };
+      // coz in auth.js we are expectiion req.cookies.token therefore the first args is token
+      res.status(200).cookie("token", token, options).json({
+        success: true,
+        token,
+        user,
+      });
+    }
+
+    res.send(400).send("email or password is incorrect");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.json({
+    message: "cookies expired successfully",
+  });
+});
+
+app.get("/dashboard", auth, (req, res) => {
+  res.send("Welcome to secret information");
+});
+
+module.exports = app;
---
+++ middleware/auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+//model is optional
+
+//hunting for token
+const auth = (req, res, next) => {
+  console.log(req.cookies);
+  const token =
+    req.cookies.token ||
+    req.body.token ||
+    req.header("Authorization").replace("Bearer ", "");
+
+  if (!token) {
+    return res.status(403).send("token is missing");
+  }
+
+  try {
+    const decode = jwt.verify(token, process.env.SECRET_KEY);
+    console.log(decode);
+    req.user = decode;
+    // bring in info DB
+  } catch (error) {
+    return res.status(401).send("Invalid token");
+  }
+
+  return next();
+};
+
+module.exports = auth;
